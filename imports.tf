bee9791a152eba30a42ab56d20fcddfbdb202890
Rob Pijnenburg <Rob.Pijnenburg@BrightCubes.nl>
2025-01-02 13:44:02 +0100

    Initial commit

---
+++ imports.tf
@@ -0,0 +1,48 @@
+# ##################################################################################
+# # IMPORTS
+# ##################################################################################
+
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-01f85ef3c3b05351c" #VPC
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-047156b7b3ebd0db2" #PublicSubnet1
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-037c0baf8ced71aaf" #PublicSubnet2
+# }
+
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-05d286b47058ac79e" #InternetGateway
+# }
+
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-07b805e5fa5f29727_0.0.0.0/0" #DefaultPublicRoute
+# }
+
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-07b805e5fa5f29727" #PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-047156b7b3ebd0db2/rtb-07b805e5fa5f29727" #PublicSubnet1/PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-037c0baf8ced71aaf/rtb-07b805e5fa5f29727" #PublicSubnet2/PublicRouteTable
+# }
+
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-041afb0289e66e6eb" #NoIngressSecurityGroup
+# }
